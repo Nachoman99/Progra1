32c59869320b5ca3f770ac99559c922f47b3b305
EstebanBV01 <>
2018-09-06 19:49:57 -0600

    haciendo pruebas

--- Progra1/src/progra1/Menu.java
+++ Progra1/src/progra1/Menu.java
@@ -1,4 +1,3 @@
- import javax.swing.JOptionPane;
 
 public class Menu {
 	public void iniciaMenu() {
@@ -6,12 +5,7 @@ public class Menu {
 		//Point point =new Point();
 		//GestorES gestor =new GestorES();
 		//arriba ewstan los atributos//////////////////////////
-		int opcion =Integer.parseInt(JOptionPane.showInputDialog(null,"seleccione una opcion\n"
-				+ "1)crear punto\n"
-				+ "2)crear linea\n"
-				+ "3)crear rectangulo\n"
-				+"4)crear triangulo"
-				));
+		int opcion=0;
 		int num1,num2;
 		double decimal1,decimal2;
 		switch (opcion) {
